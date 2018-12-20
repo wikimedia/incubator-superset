be82300b98c08442209d938faab74188b8f5082d
Andrew Otto <acotto@gmail.com>
2018-12-20 14:45:01 -0500

    Do JS webpack as part of setup.py sdist
    This allows for remote pip installs to build from source
    and get webpacked JS files transparently.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ import os
 import subprocess
 
 from setuptools import find_packages, setup
+from setuptools.command.sdist import sdist
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PACKAGE_DIR = os.path.join(BASE_DIR, 'superset', 'static', 'assets')
@@ -52,6 +53,33 @@ def parse_requirements(filename):
 INSTALL_REQUIRES = parse_requirements('requirements.txt')
 
 
+class WebpackSdistCommand(sdist):
+    """
+    Runs webpack in static/assets during sdist.
+    This will webpack up the javascript assets.
+    This should accomplish much of what ./pypi_push does.
+    """
+    def run(self):
+        import subprocess
+        # Make sure node dependencies are installed with yarn (including webpack)
+        subprocess.check_output(['yarn'], cwd=PACKAGE_DIR, stderr=subprocess.STDOUT)
+
+        # Run the npm build process to run webpack, etc.
+        # NOTE: for some reason npm run build is failing, whereas a direct
+        # webpack succeeds. Just call webpack.
+        # NOTE: Using subprocess fails, whereas os.system succeeds. Not sure why.
+        retval = os.system(
+            'cd {} && ./node_modules/.bin/webpack && cd {}'.format(PACKAGE_DIR, BASE_DIR)
+        )
+        if retval !=0:
+            raise Exception(
+                'sdist failed during webpack in {} with {}'.format(PACKAGE_DIR, retval),
+                file=sys.stderr
+            )
+
+        # Now continue the usual sdist process.
+        sdist.run(self)
+
 setup(
     name='superset',
     description=(
@@ -62,6 +90,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    # Override the usual sdist phase to run webpack first.
+    # cmdclass={'sdist': WebpackSdistCommand},
     scripts=['superset/bin/superset'],
     install_requires=INSTALL_REQUIRES,
     extras_require={
